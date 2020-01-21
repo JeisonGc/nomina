60b99230ee61153118afe4eb6dc4b7b3c90488f0
Cristhiam Monsalve <ccmonpan@hotmail.com>
2020-01-21 15:16:17 -0500

    Ajuste de parametros segun nuevo json

--- app/Parametros.php
+++ app/Parametros.php
@@ -10,11 +10,9 @@ class Parametros extends Eloquent
     use HybridRelations;
     protected $connection = 'mongodb';
     protected $collection = 'parametros';
-    /*
-    protected $casts = [
-        'contracts' => 'array',
-    ];
+    
+    
 
-    protected $fillable = ['minimum_salary','year','transport_aid','transport_aid_ms','daytime_overtime','night_overtime','sunday_hours_nocomp','daytime_overtime','sunday_night_overtime','night_surcharge'.'contracts'];
-    */
+    protected $fillable = ['minimum_salary','year','transport_aid','daytime_overtime','night_overtime','sunday_hours_nocomp','sunday_overtime','sunday_night_overtime','night_surcharge','parafiscals','health_exception','contracts','solidarity_fund','risk_class','novelties']; 
+    
 }
