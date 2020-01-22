62d2c0767e7a255a0956ced55b8b558199538db7
Silvana <silvanacastrohernandez@gmail.com>
2020-01-22 11:06:10 -0500

    Cambio nombre typeContracts en Parameter

--- app/Parameter.php
+++ app/Parameter.php
@@ -16,6 +16,6 @@ class Parameter extends Eloquent
         return $this->embedsMany('App\TypeContract');
     }
 
-    protected $fillable = ['minimum_salary','year','transport_aid','daytime_overtime','night_overtime','sunday_hours_nocomp','sunday_overtime','sunday_night_overtime','night_surcharge','parafiscals','health_exception','contracts','solidarity_fund','risk_class','novelties']; 
+    protected $fillable = ['minimum_salary','year','transport_aid','daytime_overtime','night_overtime','sunday_hours_nocomp','sunday_overtime','sunday_night_overtime','night_surcharge','parafiscals','health_exception','typeContracts','solidarity_fund','risk_class','novelties']; 
     
 }
