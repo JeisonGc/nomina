b8bf1cf301a2e386d61d53c5a3f0c88e46c79c49
Cristhiam Monsalve <ccmonpan@hotmail.com>
2020-01-23 16:37:13 -0500

    Validaciones de parametros y registro de liquidación

--- app/Http/Controllers/ParametersController.php
+++ app/Http/Controllers/ParametersController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ParametersController extends BaseController
 {
@@ -27,6 +28,7 @@ class ParametersController extends BaseController
         $parameters = Parameter::withTrashed()->get();
 
         return response()->json($parameters, 200);
+        
     }
 
 
@@ -41,6 +43,22 @@ class ParametersController extends BaseController
         $datos = $request->all();        
         $deletedRows = Parameter::where('year', $datos['year'])->delete();
        
+        $validator = Validator::make($request->all(), [            
+            'year' => 'required|numeric|min:0',
+            'daytime_overtime' => 'numeric|min:0',
+            'night_overtime' => 'numeric|min:0',
+            'sunday_hours_nocomp' => 'numeric|min:0',
+            'sunday_overtime' => 'numeric|min:0',
+            'sunday_night_overtime' => 'numeric|min:0',
+            'night_surcharge' => 'numeric|min:0',
+            'parafiscals.smlv' => 'requiredIf:parafiscals.apply,true|numeric',
+            'health_exception.smlv' => 'requiredIf:health_exception.apply,true|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $parameter = Parameter::create($request->all());
 
         return response()->json([
